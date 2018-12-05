9aa851c4953126fd8a8ac90e0df24a47f9df511a
meepdeew <meepdeew.github@gmail.com>
2018-12-05 14:55:56 -0500

    goto for C style break fallthrough

--- snippets.lisp
+++ snippets.lisp
@@ -23,12 +23,19 @@
      (return-from loop) ...
    continue))
 
+(symbol-macrolet ((break (go out)))
+  (tagbody
+     (case <expr> (<val1> (go 1)) (<val2> (go 2) ...))
+   1 <case1-code>
+   2 <case-2-code>
+   ...
+   out))
 
 (set-dispatch-macro-character
  #\# #\_
  (lambda (stream subchar arg)
    (parse-integer (remove #\_ (symbol-name (read stream nil nil t))))))
 
-(defmacro fmtf (out par-fmt &rest args)
-  (let ((fstr (format nil "~&~A~%" par-fmt)))
-    `(format ,out ,fstr ,@args)))
+(defun fmfm (par-fmt &rest rest)
+  (let ((fullstr (format nil "~~&~A" par-fmt)))
+    (apply #'format t fullstr rest)))
